4f9b44e99ecc6fb42b00d3013491a3020d5205a6
Preston Darrough <techdarrough@gmail.com>
2022-05-21 10:33:35 -0700

    Delete post failed with cors error switch to cors

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const { Sequelize } =  require('sequelize');
-const request =
+
 
 //Config / Mid-ware
 require('dotenv').config() // loads config file .enc
+app.use(cors()); // prevent cors errors
 app.use(express.json()) //allows access to req.body
 app.use(express.urlencoded({ extended: false })) // parsing data sent to server via put or post
 
 // cors proxy - insert response into all headers to allow for cors
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+// !!!!!! SWITCHED to cors as delete would fail on cors error
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
 
 app.get('/', (req, res)=>{
     res.status(200).json({
